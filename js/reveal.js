a91ed19d1d4eb9e7c511c41b5a57ba922cc767fb
Joshua Holmes <hello@joshuaholmes.id.au>
2016-04-23 00:29:29 +1000

    Initial commit
    New website commit

---
+++ js/reveal.js
@@ -0,0 +1,8 @@
+function reveal() {
+    document.getElementById("name").textContent = "";
+    document.getElementById("name").innerHTML = "<img src='images/aurebesh.png'>";
+}
+
+ $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
